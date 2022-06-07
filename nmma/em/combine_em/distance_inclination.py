02403813e96ded42a1b9c0282ddf1950f9721abd
Andrew Michael Toivonen <andrew.toivonen@ldas-pcdev6.ligo.caltech.edu>
2022-06-07 15:50:26 -0700

    bug fixes

--- nmma/em/combine_em/distance_inclination.py
+++ nmma/em/combine_em/distance_inclination.py
@@ -6,7 +6,7 @@ from gwpy.table import Table
 from combine_em_utils import KDE_multiply, logit
 
 
-def combine_EM_events(original_KDE, EM_event, D_range = [0,1000], i_range = [0,1], transform_type=logit):
+def combine_EM_events(original_KDE, EM_event, D_range = [0,1000], i_range = [0,1], transform_type=logit, downsample=False):
     '''
     '''
     D = EM_event['luminosity_distance']
@@ -19,7 +19,7 @@ def combine_EM_events(original_KDE, EM_event, D_range = [0,1000], i_range = [0,1
     #print(np.min(i), np.max(i), np.mean(i), np.median(i))
     #print('-------------')
     #print(np.min(i_original), np.max(i_original), np.mean(i_original), np.median(i_original))
-
+    print('Transforming...')
     D_transform, D_prior = logit(D, D_range, include_prior=True)
     i_transform, i_prior = logit(i, i_range, include_prior=True) 
     #D_original_transform = logit(D_original, D_range)
@@ -32,9 +32,11 @@ def combine_EM_events(original_KDE, EM_event, D_range = [0,1000], i_range = [0,1
 
     D_kde_original = original_KDE['luminosity_distance']
     i_kde_original = original_KDE['cos_inclination_EM']
+    print(D_kde_original, i_kde_original)
 
-    D_joint_kde = KDE_multiply(D_kde_transform, D_kde_original)
-    i_joint_kde = KDE_multiply(i_kde_transform, i_kde_original)
+    print('Taking joint KDE')
+    D_joint_kde = KDE_multiply(D_kde_transform, D_kde_original, downsample=True)
+    i_joint_kde = KDE_multiply(i_kde_transform, i_kde_original, downsample=True)
 
     '''
     D_transform_resam = D_jkde.resample(len(D))
@@ -51,7 +53,7 @@ def combine_EM_events(original_KDE, EM_event, D_range = [0,1000], i_range = [0,1
         plt.savefig('./output/i_hist.png')
         plt.close()
     '''
-
+    print('Done!')
     return D_joint_kde, i_joint_kde
 
 def run_event_combination(EM_event_files, save_hist=False, N_hist = 1000):
@@ -60,8 +62,9 @@ def run_event_combination(EM_event_files, save_hist=False, N_hist = 1000):
     original_KDE = EM_event_files[0]
     for n, event in enumerate(EM_event_files[1:]):
         print('Combining events '+str(n+1)+' and '+str(n+2))
-        D_kde, i_kde = combine_EM_events(original_KDE, event)
+        D_kde, i_kde = combine_EM_events(original_KDE, event, downsample=False)
         original_KDE = {'luminosity_distance': D_kde, 'cos_inclination_EM': i_kde}
+        print('Events combined!')
 
     D_transform_resam = D_kde.resample(N_hist)
     i_transform_resam = i_kde.resample(N_hist)
@@ -70,11 +73,13 @@ def run_event_combination(EM_event_files, save_hist=False, N_hist = 1000):
     i_resam = logit(i_transform_resam, i_range, inv_transform=True)
 
     if save_hist:
-        D_hist = plt.hist(D_resam)
+        plt.hist(D_resam.flatten(), bins=20)
+        #plt.savefig('./output/D_downsample_hist.png')
         plt.savefig('./output/D_hist.png')
         plt.close()
-        i_hist = plt.hist(i_resam)
-        plt.savefig('./output/i_hist.png')
+        plt.hist(i_resam.flatten(), bins=20)
+        #plt.savefig('./output/i_downsample_hist.png')
+        plt.savefig('./output/D_hist.png')
         plt.close()
 
 
@@ -108,5 +113,6 @@ original_KDE['cos_inclination_EM'] = i_kde_transform
 
 #em_list = ['', '1', '2', '3', '4', '10']
 event_list = [original_KDE, samples2, samples3, samples4]
+#event_list = [original_KDE, samples3, samples4]
 
 run_event_combination(event_list, save_hist=True)
