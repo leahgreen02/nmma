e322699ae32bdd6ed62a5aac6e5a1ad0c6089d38
Andrew Michael Toivonen <andrew.toivonen@ldas-pcdev6.ligo.caltech.edu>
2022-07-18 09:13:04 -0700

    First working implementation for H0

--- nmma/em/combine_em/calc_H0.py
+++ nmma/em/combine_em/calc_H0.py
@@ -2,10 +2,13 @@ import numpy as np
 from astropy.cosmology import Planck18
 from astropy import units as u
 from astropy.coordinates import Distance
+from astropy import constants as const
 import matplotlib.pyplot as plt
 import scipy.stats
 import pandas as pd
 import joypy
+import json
+import bilby
 from gwpy.table import Table
 from combine_em_utils import KDE_multiply, logit
 from bilby.gw.conversion import luminosity_distance_to_redshift
@@ -16,17 +19,15 @@ c = 3e5 # km/s
 # z ~ H0 * D / c
 # H0 = z * c /D
 H_true = Planck18.H(0)
-print(H_true)
+c = const.c.to('km/s').value
 
-
-def combine_EM_events(original_KDE, EM_event, H_range = [0,150], D_range = [0,200], i_range = [0,1], transform_type=logit, downsample=True):
+def combine_EM_events(original_KDE, EM_event, D_original_inj_D, D_inj, H_range = [0,150], D_range = [0,1000], i_range = [0,1], transform_type=logit, downsample=True):
     '''
     '''
     D = EM_event['luminosity_distance']
     i = EM_event['cos_inclination_EM']
-
-    z = np.array(luminosity_distance_to_redshift(D, cosmology=Planck18))
-
+    # use injection distance
+    z = np.array(luminosity_distance_to_redshift(D_inj, cosmology=Planck18))
     H = z * c / D
 
     print('Transforming...')
@@ -34,10 +35,10 @@ def combine_EM_events(original_KDE, EM_event, H_range = [0,150], D_range = [0,20
     D_transform, D_prior = logit(D, D_range, include_prior=True)
     i_transform, i_prior = logit(i, i_range, include_prior=True) 
 
+    print('Taking KDE...')
     H_kde_transform = scipy.stats.gaussian_kde(H_transform, weights=1/H_prior)
     D_kde_transform = scipy.stats.gaussian_kde(D_transform, weights=1/D_prior)
     i_kde_transform = scipy.stats.gaussian_kde(i_transform, weights=1/i_prior)
-
     H_kde_original = original_KDE['H0']
     D_kde_original = original_KDE['luminosity_distance']
     i_kde_original = original_KDE['cos_inclination_EM']
@@ -55,9 +56,16 @@ def run_event_combination(EM_event_files, save_hist=False, N_hist = 1000):
     '''
     '''
     original_KDE = EM_event_files[0]
+    with open('../../../injection.json', 'r') as f:
+        injection_dict = json.load(f, object_hook=bilby.core.utils.decode_bilby_json)
+    injection_df = injection_dict["injections"]
+    original_injection_parameters = injection_df.iloc[0].to_dict() 
+    original_injected_D = original_injection_parameters['luminosity_distance']
     for n, event in enumerate(EM_event_files[1:]):
         print('Combining events '+str(n+1)+' and '+str(n+2))
-        D_kde, i_kde, H_kde = combine_EM_events(original_KDE, event)
+        injection_parameters = injection_df.iloc[n+1].to_dict()
+        injected_D = injection_parameters['luminosity_distance']
+        D_kde, i_kde, H_kde = combine_EM_events(original_KDE, event, original_injected_D, injected_D)
         original_KDE = {'luminosity_distance': D_kde, 'cos_inclination_EM': i_kde, 'H0': H_kde}
         print('Events combined!')
 
@@ -69,52 +77,28 @@ def run_event_combination(EM_event_files, save_hist=False, N_hist = 1000):
     D_resam = logit(D_transform_resam, D_range, inv_transform=True)
     i_resam = logit(i_transform_resam, i_range, inv_transform=True)
 
-    if save_hist:
-        plt.hist(H_resam.flatten(), bins=20)
-        plt.xlabel('H_0')
-        plt.ylabel('Probability')
-        plt.savefig('./output/H_downsample_hist.png')
-        #plt.savefig('./output/H_hist.png')
-        plt.close()
-        plt.hist(D_resam.flatten(), bins=20)
-        plt.xlabel('Luminosity Distance')
-        plt.ylabel('Probability')
-        plt.savefig('./output/D_downsample_hist.png')
-        #plt.savefig('./output/D_hist.png')
-        plt.close()
-        plt.hist(i_resam.flatten(), bins=20)
-        plt.xlabel('Cos Inclination Angle')
-        plt.ylabel('Probability')
-        plt.savefig('./output/i_downsample_hist.png')
-        #plt.savefig('./output/D_hist.png')
-        plt.close()
     return D_resam.flatten(), i_resam.flatten(), H_resam.flatten()
 
-'''
-samples1 = Table.read('../../../outdir/injection_posterior_samples.dat', format="csv", delimiter=" ")
+with open('../../../injection.json', 'r') as f:
+    injection_dict = json.load(f, object_hook=bilby.core.utils.decode_bilby_json)
+injection_df = injection_dict["injections"]
+injection_parameters = injection_df.iloc[0].to_dict()
+true_D1 = injection_parameters['luminosity_distance']
+injection_parameters = injection_df.iloc[1].to_dict()
+true_D2 = injection_parameters['luminosity_distance']
+injection_parameters = injection_df.iloc[2].to_dict()
+true_D3 = injection_parameters['luminosity_distance']
+injection_parameters = injection_df.iloc[3].to_dict()
+true_D4 = injection_parameters['luminosity_distance']
+
+samples1 = Table.read('../../../outdir0_custom/injection_posterior_samples.dat', format="csv", delimiter=" ")
 samples1['cos_inclination_EM'] = np.cos(samples1['inclination_EM'])
 
-samples2 = Table.read('../../../outdir2/injection_posterior_samples.dat', format="csv", delimiter=" ")
+samples2 = Table.read('../../../outdir1_custom/injection_posterior_samples.dat', format="csv", delimiter=" ")
 samples2['cos_inclination_EM'] = np.cos(samples2['inclination_EM'])
 
-samples3 = Table.read('../../../outdir3/injection_posterior_samples.dat', format="csv", delimiter=" ")
+samples3 = Table.read('../../../outdir2_custom/injection_posterior_samples.dat', format="csv", delimiter=" ")
 samples3['cos_inclination_EM'] = np.cos(samples3['inclination_EM'])
-
-samples4 = Table.read('../../../outdir4/injection_posterior_samples.dat', format="csv", delimiter=" ")
-samples4['cos_inclination_EM'] = np.cos(samples4['inclination_EM'])
-'''
-
-samples1 = Table.read('../../../outdir_new1/injection_posterior_samples.dat', format="csv", delimiter=" ")
-samples1['cos_inclination_EM'] = np.cos(samples1['inclination_EM'])
-
-samples2 = Table.read('../../../outdir_new2/injection_posterior_samples.dat', format="csv", delimiter=" ")
-samples2['cos_inclination_EM'] = np.cos(samples2['inclination_EM'])
-
-samples3 = Table.read('../../../outdir_new3/injection_posterior_samples.dat', format="csv", delimiter=" ")
-samples3['cos_inclination_EM'] = np.cos(samples3['inclination_EM'])
-
-samples4 = Table.read('../../../outdir_new4/injection_posterior_samples.dat', format="csv", delimiter=" ")
-samples4['cos_inclination_EM'] = np.cos(samples4['inclination_EM'])
 
 D = samples1['luminosity_distance']
 i = samples1['cos_inclination_EM']
@@ -125,21 +109,16 @@ i2 = samples2['cos_inclination_EM']
 D3 = samples3['luminosity_distance']
 i3 = samples3['cos_inclination_EM']
 
-D4 = samples4['luminosity_distance']
-i4 = samples4['cos_inclination_EM']
-
-D_range = [0,200] 
+D_range = [0,1000] 
 i_range = [0,1]
 H_range = [0,150]
 
-z = np.array(luminosity_distance_to_redshift(D))
+z = np.array(luminosity_distance_to_redshift(true_D1))
 H = z * c / D
 z2 = np.array(luminosity_distance_to_redshift(D2))
 H2 = z2 * c / D2
 z3 = np.array(luminosity_distance_to_redshift(D3))
 H3 = z3 * c / D3
-z4 = np.array(luminosity_distance_to_redshift(D4))
-H4 = z4 * c / D4
 
 H_transform, H_prior = logit(H, H_range, include_prior=True)
 D_transform, D_prior = logit(D, D_range, include_prior=True)
@@ -155,63 +134,42 @@ original_KDE['cos_inclination_EM'] = i_kde_transform
 original_KDE['H0'] = H_kde_transform
 
 #em_list = ['', '1', '2', '3', '4', '10']
-event_list = [original_KDE, samples2, samples3, samples4]
+event_list = [original_KDE, samples2, samples3]
 #event_list = [original_KDE, samples1, samples2]
 
 D_combined, i_combined, H_combined = run_event_combination(event_list, N_hist=len(D), save_hist=True)
-tab = Table([D_combined, D], names=['Combined Events', 'Event1'])
+#tab = Table([D_combined, D], names=['Combined Events', 'Event1'])
 
-plt.hist(D, alpha=.5, label = 'Event1')
-plt.hist(D2, alpha=.5, label = 'Event2')
-plt.hist(D3, alpha=.5, label = 'Event3')
-plt.hist(D4, alpha=.5, label = 'Event4')
-plt.hist(D_combined, label = 'Combined')
+plt.hist([D, D2, D3], alpha=.8, bins = 30, histtype='stepfilled', density=True, label = ['Event1', 'Event2', 'Event3', 'Event4'])
 plt.ylabel('Probability')
 plt.xlabel('Luminosity Distance')
+plt.axvline(x=true_D1, color='blue', label='True D1')
+plt.axvline(x=true_D2, color='orange', label='True D2')
+plt.axvline(x=true_D3, color='green', label='True D3')
 plt.legend()
 plt.savefig('./output/comparison_hist_D.png')
 plt.close()
 
-plt.hist(i, alpha=.5, label = 'Event1')
-plt.hist(i2, alpha=.5, label = 'Event2')
-plt.hist(i3, alpha=.5, label = 'Event3')
-plt.hist(i4, alpha=.5, label = 'Event4')
-plt.hist(i_combined, label = 'Combined')
-plt.ylabel('Probability')
-plt.xlabel('Cos Inclination Angle')
-plt.legend()
-plt.savefig('./output/comparison_hist_i.png')
-plt.close()
+#H = z * c / D
+z = np.array(luminosity_distance_to_redshift(D, cosmology=Planck18))
+z2 = np.array(luminosity_distance_to_redshift(D2, cosmology=Planck18))
+z3 = np.array(luminosity_distance_to_redshift(D3, cosmology=Planck18))
+H_test1 = z * c / D
+H_test2 = z2 * c / D2
+H_test3 = z3 * c / D3
 
-plt.hist(H, alpha=.5, label = 'Event1')
-plt.hist(H2, alpha=.5, label = 'Event2')
-plt.hist(H3, alpha=.5, label = 'Event3')
-plt.hist(H4, alpha=.5, label = 'Event4')
-plt.hist(H_combined, label = 'Combined')
-plt.ylabel('Probability')
-plt.xlabel('H_0')
-plt.legend()
-plt.savefig('./output/comparison_hist_H.png')
-plt.close()
-
-plt.hist([D, D2, D3, D4], alpha=.8, bins = 30, stacked = True, density=True, label = ['Event1', 'Event2', 'Event3', 'Event4'])
-plt.ylabel('Probability')
-plt.xlabel('Luminosity Distance')
-plt.legend()
-plt.savefig('./output/comparison_hist2_D.png')
-plt.close()
-
-plt.hist([H, H2, H3, H4, H_combined], alpha=.8, bins = 30, stacked = True, density=True, label = ['Event1', 'Event2', 'Event3', 'Event4', 'Combined'])
+plt.hist([H, H2, H3, H_combined], alpha=.8, bins = 30, histtype = 'stepfilled', density=True, label = ['Event1', 'Event2', 'Event3', 'Combined'])
 plt.ylabel('Probability')
 plt.xlabel('H_0')
 plt.axvline(x=H_true.value, color='black', label='Planck18 H_0') # H_true
 plt.legend()
-plt.savefig('./output/comparison_hist2_H.png')
+plt.savefig('./output/comparison_stacked_hist_H.png')
 plt.close()
 
-plt.hist([H, H2, H3, H4, H_combined], alpha=.8, bins=30, histtype='step', density=True, label = ['Event1', 'Event2', 'Event3', 'Event4', 'Combined'])
+plt.hist([H, H2, H3, H_combined], alpha=.8, bins=30, histtype='step', density=True, linewidth=2, label = ['Event1', 'Event2', 'Event3', 'Combined'])
 plt.ylabel('Probability')
 plt.xlabel('H_0')
+plt.axvline(x=H_true.value, color='black', label='Planck18 H_0') # H_true
 plt.legend()
-plt.savefig('./output/comparison_hist3_H.png')
+plt.savefig('./output/comparison_line_hist_H.png')
 plt.close()
