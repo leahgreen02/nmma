da741249b6e762e1617d61f5bf6af9d1a879e5b2
Andrew Michael Toivonen <andrew.toivonen@ldas-pcdev4.ligo.caltech.edu>
2022-12-13 12:28:37 -0800

    GW170817 prior

---
+++ nmma/em/combine_em/calc_H0_GW170817.py
@@ -0,0 +1,195 @@
+import os
+import numpy as np
+from astropy.cosmology import Planck18
+from astropy import units as u
+from astropy.coordinates import Distance
+from astropy import constants as const
+import matplotlib.pyplot as plt
+import scipy.stats
+import pandas as pd
+import h5py
+#import joypy
+import json
+import bilby
+from gwpy.table import Table
+from combine_em_utils import KDE_multiply, logit, calc_H0
+from bilby.gw.conversion import luminosity_distance_to_redshift
+
+# v = H0*D
+#c = 3e5 # km/s
+# z ~ H0 * D / c
+# H0 = z * c /D
+H_true = Planck18.H(0)
+c = const.c.to('km/s').value
+
+with h5py.File('GW170817_PE.hdf5', 'r') as data:
+    GW170817_samples = data['posterior_samples'][()]
+
+
+def combine_EM_events(original_KDE, EM_event, original_D_inj, D_inj, D_prior_kde, H_range, D_range, i_range, transform_type=logit, downsample=True, num=None):
+    '''
+    '''
+    D = EM_event['luminosity_distance']
+    print(min(D), max(D), np.median(D))
+    print(D_inj)
+    i = EM_event['cos_inclination_EM']
+    # use injection distance
+    z = np.array(luminosity_distance_to_redshift(D_inj, cosmology=Planck18))
+    
+    N_hist = len(D)
+    #z = z + z*.2*(2*np.random.rand(N_hist)-1)
+
+    D = D + D*.2*(2*np.random.rand(N_hist)-1)
+    H = calc_H0(D, z)
+
+
+    plt.figure(figsize=(12,9))
+    plt.hist(H)
+    plt.ylabel('Probability')
+    plt.xlabel('H_0')
+    plt.axvline(x=H_true.value, color='black', label='Planck18 H_0') # H_true
+    plt.legend()
+    plt.savefig(f'./output/H_individual_hist_uniform_source_frame_H0_no_logit_{num}.png')
+    plt.close() 
+
+    # D_prior = bilby.gw.prior.UniformSourceFrame(minimum=0.0, maximum=2000., name='luminosity_distance',latex_label='$D_L$')
+    # D_prior = D_prior.prob(D)
+
+    #D_prior_kde = scipy.stats.gaussian_kde(GW170817_samples['luminosity_distance'])
+    D_prior = D_prior_kde.resample(N_hist)[0]
+
+    H_transform, H_prior = logit(H, H_range, include_prior=True)
+    #D_transform, D_prior = logit(D, D_range, include_prior=True)
+    i_transform, i_prior = logit(i, i_range, include_prior=True) 
+
+    H_kde_transform = scipy.stats.gaussian_kde(H_transform, weights=1/H_prior)
+    D_kde = scipy.stats.gaussian_kde(D, weights=1/D_prior)
+    i_kde_transform = scipy.stats.gaussian_kde(i_transform, weights=1/i_prior)
+    H_kde_original = original_KDE['H0']
+    D_kde_original = original_KDE['luminosity_distance']
+    i_kde_original = original_KDE['cos_inclination_EM']
+
+    H_joint_kde = KDE_multiply(H_kde_transform, H_kde_original, downsample=downsample)
+    D_joint_kde = KDE_multiply(D_kde, D_kde_original, downsample=downsample)
+    i_joint_kde = KDE_multiply(i_kde_transform, i_kde_original, downsample=downsample)
+
+    return D_joint_kde, i_joint_kde, H_joint_kde
+
+def run_event_combination(path_to_events, H_range, D_range, i_range, save_hist=False):
+    '''
+    '''
+    # note, right now the list dir MUST read files in the same order as the injections are in the injection file
+    event_folders = os.listdir(path_to_events)
+    EM_event_files = []
+    event_indices = []
+    for folder in event_folders:
+        N = int(folder[-1])
+        samples = Table.read(f'{path_to_events}/{folder}/injection_posterior_samples.dat', format="csv", delimiter=" ")
+        samples['cos_inclination_EM'] = np.cos(samples['inclination_EM'])
+        EM_event_files.append(samples)
+        event_indices.append(N)
+
+    print(event_folders)
+    print(event_indices)
+    with open('../../../injection_usf_1000.json', 'r') as f:
+        injection_dict = json.load(f, object_hook=bilby.core.utils.decode_bilby_json)
+    injection_df = injection_dict["injections"]
+    original_injection_parameters = injection_df.iloc[event_indices[0]].to_dict()
+    original_D_inj = original_injection_parameters['luminosity_distance'] # working
+    #original_D_inj = 40
+    #original_D_inj = original_D_inj + original_D_inj*.2*(2*np.random.rand(10)-1)
+
+    original_samples = EM_event_files[0]
+    D = original_samples['luminosity_distance'] # working
+    #D = GW170817_samples['luminosity_distance']
+    i = original_samples['cos_inclination_EM']
+
+    N_hist = len(D)
+
+    D = D + D*.2*(2*np.random.rand(N_hist)-1)
+
+    z = np.array(luminosity_distance_to_redshift(original_D_inj, cosmology=Planck18))
+    #z = z + z*.2*(2*np.random.rand(N_hist)-1)
+    H = calc_H0(D, z)
+
+    # D_prior = bilby.gw.prior.UniformSourceFrame(minimum=0.0, maximum=2000., name='luminosity_distance', cosmology = Planck18, latex_label='$D_L$') # working
+    # D_prior = D_prior.prob(D) # working 
+    D_prior_kde = scipy.stats.gaussian_kde(GW170817_samples['luminosity_distance'])
+    D_prior = D_prior_kde.resample(N_hist)[0]
+
+    H_transform, H_prior = logit(H, H_range, include_prior=True)
+    #D_transform, D_prior = logit(D, D_range, include_prior=True)
+    i_transform, i_prior = logit(i, i_range, include_prior=True)
+
+    H_kde_transform = scipy.stats.gaussian_kde(H_transform, weights=1/H_prior)
+    D_kde = scipy.stats.gaussian_kde(D, weights=1/D_prior)
+    i_kde_transform = scipy.stats.gaussian_kde(i_transform, weights=1/i_prior)
+
+    original_KDE = {}
+    original_KDE['luminosity_distance'] = D_kde
+    original_KDE['cos_inclination_EM'] = i_kde_transform
+    original_KDE['H0'] = H_kde_transform
+
+    H_medians = []
+    H_stds = []
+    H_medians.append(np.median(H))
+    H_stds.append(np.std(H))
+
+    N_hist = len(D)
+
+    count = 2
+    for n, event in zip(event_indices[1:], EM_event_files[1:]):
+        print(f'Combining events')
+        injection_parameters = injection_df.iloc[n].to_dict()
+        D_inj = injection_parameters['luminosity_distance']
+        #D_inj = D_inj + D_inj*.2*(2*np.random.rand(10)-1)
+        D_kde_result, i_kde, H_kde = combine_EM_events(original_KDE, event, original_D_inj, D_inj, D_prior_kde, H_range = H_range, D_range = D_range, i_range = i_range, num=n)
+        original_KDE = {'luminosity_distance': D_kde_result, 'cos_inclination_EM': i_kde, 'H0': H_kde}
+        H_vals = logit(H_kde.resample(N_hist), H_range, inv_transform=True)
+        H_medians.append(np.median(H_vals))
+        H_stds.append(np.std(H_vals))
+        print(f'{count} events combined!')
+        count+=1
+
+    H_transform_resam = H_kde.resample(N_hist)
+    D_transform_resam = D_kde_result.resample(N_hist)
+    i_transform_resam = i_kde.resample(N_hist)
+
+    H_resam = logit(H_transform_resam, H_range, inv_transform=True)
+    #D_resam = logit(D_transform_resam, D_range, inv_transform=True)
+    D_resam = D_transform_resam
+    i_resam = logit(i_transform_resam, i_range, inv_transform=True)
+
+    return D_resam.flatten(), i_resam.flatten(), H_resam.flatten(), np.array(H_medians), np.array(H_stds)
+
+
+path_to_events = '../../../usf_runs'
+
+D_combined, i_combined, H_combined, H_medians, H_stds = run_event_combination(path_to_events, H_range = [0,200], D_range = [0,2000], i_range = [0,1], save_hist=True)
+
+# histtype='stepfilled'
+plt.figure(figsize=(12,9))
+plt.hist(H_combined, alpha=.8, bins = 30, edgecolor = 'black', density=True, label = 'Combined Events')
+plt.ylabel('Probability')
+plt.xlabel(r'H$_{0}$ [km/s/Mpc]')
+plt.axvline(x=H_true.value, color='red', linestyle='--', linewidth=4, label=r'Planck18 H$_{0}$') # H_true
+plt.legend()
+plt.savefig('./output/H0_combined_hist_usf.png')
+plt.close()
+
+print(H_stds, H_medians)
+
+#plt.figure(figsize=(12,9))
+f, (ax0, ax1) = plt.subplots(2, 1, figsize=(12,9), sharex=True, gridspec_kw={'height_ratios': [3, 1]})
+#plt.hist(H_combined, alpha=.8, bins = 30, edgecolor = 'black', density=True, label = 'Combined Events')
+ax1.plot(np.arange(1,len(H_medians)+1), H_stds/H_true.value, linewidth=3, marker='o')
+#plt.plot(np.arange(1,11), H_stds, marker='o')
+ax0.errorbar(np.arange(1,len(H_medians)+1), H_medians, yerr=H_stds, linewidth=3, capsize=6, label='Combined Event Prediction')
+ax0.axhline(y=H_true.value, color='red', linestyle='--', linewidth=2, label=r'Planck18 H$_{0}$')
+ax0.set_ylabel(r'H$_{0}$ [km/s/Mpc]')
+ax1.set_ylabel(r'Fractional H$_{0}$ Error')
+plt.xlabel(r'Number of Events')
+#plt.axvline(x=H_true.value, color='red', linestyle='--', linewidth=4, label=r'Planck18 H$_{0}$') # H_true
+ax0.legend()
+plt.savefig('./output/H0_error.png')
+plt.close()
